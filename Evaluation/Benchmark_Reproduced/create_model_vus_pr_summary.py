0c0d9340ae9750a5e9aa11df7d5df5dad2afd660
Kai <spaier.p@gmail.com>
2025-06-08 18:44:28 +0200

    dd

--- Evaluation/Benchmark_Reproduced/create_model_vus_pr_summary.py
+++ Evaluation/Benchmark_Reproduced/create_model_vus_pr_summary.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 # Read the input CSV file
-input_file = 'Evaluation/Benchmark_Reproduced/merged_all_multi_metrics.csv'
+input_file = 'Evaluation/Benchmark_Reproduced/merged_all_uni_metrics.csv'
 df = pd.read_csv(input_file)
 
 # Pivot the data to have models as columns and VUS-PR as values
@@ -12,7 +12,7 @@ pivoted_df = df.pivot(index='file', columns='model', values='VUS-PR')
 pivoted_df = pivoted_df.reset_index()
 
 # Save to a new CSV file
-output_file = 'Evaluation/Benchmark_Reproduced/model_vus_pr_summary_multi.csv'
+output_file = 'Evaluation/Benchmark_Reproduced/model_vus_pr_summary_uni.csv'
 pivoted_df.to_csv(output_file, index=False)
 
 print(f"Created summary file: {output_file}") 
